174b0940032cacbd67bd8443b6e2e0c1ecdfd145
Jlima2016306 <69653451+Jlima2016306@users.noreply.github.com>
2021-06-14 14:35:00 -0600

    Menu

--- src/app/component/inicio/inicio.component.ts
+++ src/app/component/inicio/inicio.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { GLOBAL } from 'src/app/service/global.service';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
+  providers:[UsuarioService]
+
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
-
+  public identidad;
+  public url;
+  constructor(
+    public _usuarioService: UsuarioService,
+  ) {
+    this.identidad = this._usuarioService.obtenerIdentidad()
+    this.url = GLOBAL.url
+  }
   ngOnInit(): void {
   }
 
 }
+
+
+
+
+
+
